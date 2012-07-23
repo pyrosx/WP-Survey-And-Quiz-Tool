7947340f66ef9e5754fca22e820d3b7d29c767e8
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-07-23 10:42:03 +0100

    Check array key exists and remove confusing line

--- pages/admin/questions/index.php
+++ pages/admin/questions/index.php
@@ -120,13 +120,12 @@ var saveOrder = function() {
                         $rawResult['sections'] = unserialize($rawResult['sections']);
                         
                         foreach((array)$rawResult['sections'] as $result_sections){
-                            if($result_sections['answers'][$question['id']]['mark'] == 'correct') $correct_answer++;
+                            if(isset($result_sections['answers'][$question['id']]['mark']) && ($result_sections['answers'][$question['id']]['mark'] == 'correct')) $correct_answer++;
                         }
                     }
                     $success_rate = number_format(($correct_answer/$RESULTS_count)*100, 0);
                     //echo ($success_rate);
                     echo ($success_rate<80 ? "<span style=\"color:red;\">$success_rate%</span>" : "<span style=\"color:green;\">$success_rate%</span>");
-                    echo 'Out of:' . $correct_answer .'/'. $RESULTS_count;
                     ?>
                 </td>
                 
