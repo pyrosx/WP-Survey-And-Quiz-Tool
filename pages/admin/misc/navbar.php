625eb5eb537f2193d360c11b686e3a40e51ba000
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-07-23 16:25:41 +0100

    Show shortcode on edit page

--- pages/admin/misc/navbar.php
+++ pages/admin/misc/navbar.php
@@ -10,6 +10,9 @@ if ( isset($_GET['id']) ){
 	$quizName = $wpdb->get_var(
 					$wpdb->prepare("SELECT name FROM `".WPSQT_TABLE_QUIZ_SURVEYS."` WHERE id = %s", array($_GET['id']))
 							 );
+	$quizType = $wpdb->get_var(
+					$wpdb->prepare("SELECT type FROM `".WPSQT_TABLE_QUIZ_SURVEYS."` WHERE id = %s", array($_GET['id']))
+							 );
 	
 	?>
 	<div>
@@ -20,6 +23,7 @@ if ( isset($_GET['id']) ){
 			<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=questions&subsection=<?php esc_html_e($_GET["subsection"]); ?>&id=<?php esc_html_e($_GET["id"]); ?>"<?php if ( wpsqt_is_section('questions') ) { ?> class="current"<?php }?>>Questions</a> | </li>  
 			<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=form&subsection=<?php esc_html_e($_GET['subsection']); ?>&id=<?php esc_html_e($_GET["id"]); ?>"<?php if ( wpsqt_is_section('form') ) { ?> class="current"<?php }?>>Form</a> | </li> 
 			<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=results&subsection=<?php esc_html_e($_GET["subsection"]); ?>&id=<?php esc_html_e($_GET["id"]); ?>"<?php if ( wpsqt_is_section('results') ) { ?> class="current"<?php }?>>Results</a></li> 
+			<li style="padding-left: 30px;">Shortcode: <pre style="display: inline;">[wpsqt name="<?=$quizName?>" type="<?=$quizType?>"]</pre></li>
 		</ul>
 		<div style="clear:both;"></div>
 	</div>
