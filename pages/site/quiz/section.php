e988189098cf6796a95828b0f2d6d9a967d7efff
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-09-13 17:54:04 +0100

    Added translation for quiz submit button

--- pages/site/quiz/section.php
+++ pages/site/quiz/section.php
@@ -75,7 +75,7 @@ foreach ($_SESSION['wpsqt'][$quizName]['sections'][$sectionKey]['questions'] as 
 <?php } ?>
 <?php
 if ($sectionKey == (count($_SESSION['wpsqt'][$quizName]['sections']) - 1)) {
-	?><p><input type='submit' value='Submit' class='button-secondary' /></p><?php
+	?><p><input type='submit' value='<?php _e('Submit', 'wp-survey-and-quiz-tool'); ?>' class='button-secondary' /></p><?php
 } else {
 	?><p><input type='submit' value='<?php _e('Next', 'wp-survey-and-quiz-tool'); ?> &raquo;' class='button-secondary' /></p><?php
 }
