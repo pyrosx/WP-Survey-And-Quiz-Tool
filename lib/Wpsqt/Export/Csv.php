d8f92b518d4be63818ef4277e979e70f085c49cd
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-09-20 13:38:15 +0100

    Work on generating csv

--- lib/Wpsqt/Export/Csv.php
+++ lib/Wpsqt/Export/Csv.php
@@ -21,5 +21,22 @@ class Wpsqt_Export_Csv extends Wpsqt_Export {
 		
 		return $csv;	
 	}		
+
+	public function generate($id) {
+		global $wpdb;
+
+		$results = $wpdb->get_results('SELECT * FROM '.WPSQT_TABLE_RESULTS.' WHERE item_id = "'.$id.'"', ARRAY_A);
+
+		/*foreach( $results as $result ){ 
+			$csvline =  $result['id'].","; 
+			if($result['total'] == 0) {$csvline .= ',,';} else {$csvline .= $result['score'].",".$result['total'].",";}
+			if($result['total'] == 0) {$csvline .= ',';} else {$csvline .= $result['percentage']."%,";}
+			if ($result['pass'] == 1) {$csvline .= "Pass,";} else {$csvline .= "Fail,";}
+			$csvline .= ucfirst($result['status']).","; 
+			if (!empty($result['datetaken'])) { $csvline .= date('d-m-y G:i:s',$result['datetaken']); }; 
+			$csvlines[] = $csvline;
+		} */
+
+	}
 		
 }
