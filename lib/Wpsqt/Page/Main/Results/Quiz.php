4c30adc3c92bdbb247ecb7cebffa55a6a3ace727
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-09-20 15:47:44 +0100

    Fix continuous exporting after one export

--- lib/Wpsqt/Page/Main/Results/Quiz.php
+++ lib/Wpsqt/Page/Main/Results/Quiz.php
@@ -14,8 +14,10 @@ class Wpsqt_Page_Main_Results_Quiz extends Wpsqt_Page_Main_Results {
 			$path = $csvExporter->saveFile();
 
 			echo '<iframe src='.plugins_url($path, WPSQT_FILE).' style="display:none;"></iframe>';
+			$this->redirect(WPSQT_URL_MAIN.'&section=results&subsection=quiz&id='.$_GET['id']);
+		} else {
+			$this->_pageView = 'admin/results/index.php';
 		}
-		$this->_pageView = 'admin/results/index.php';
 	}	
 	
 }
