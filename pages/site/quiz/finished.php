92bd615af932c77e08f9c4b60186f18971f50276
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-12-06 17:30:31 +0000

    Allow quiz review and finish message

--- pages/site/quiz/finished.php
+++ pages/site/quiz/finished.php
@@ -8,26 +8,28 @@ $objTokens->setDefaultValues();
 
 <h2><?php _e('Exam Finished', 'wp-survey-and-quiz-tool'); ?></h2>
 
-<?php if ($_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Finish message'  ) { ?>
+<?php if ($_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Finish message' || $_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Both'  ) { ?>
 	<?php if (isset($_SESSION['wpsqt'][$quizName]['details']['pass_finish']) &&
-				$_SESSION['wpsqt'][$quizName]['details']['pass_finish'] == "yes" &&
-				$percentRight >= $_SESSION['wpsqt'][$quizName]['details']['pass_mark']) {
-					$string = $objTokens->doReplacement($_SESSION['wpsqt'][$quizName]['details']['pass_finish_message']);
-					echo nl2br($string);
-				} else if ( isset($_SESSION['wpsqt'][$quizName]['details']['fail_review']) &&
-					$_SESSION['wpsqt'][$quizName]['details']['fail_review'] == "yes" &&
-					$percentRight < $_SESSION['wpsqt'][$quizName]['details']['pass_mark']) {
-					require_once Wpsqt_Core::pageView('site/quiz/review.php');
+		$_SESSION['wpsqt'][$quizName]['details']['pass_finish'] == "yes" &&
+		$percentRight >= $_SESSION['wpsqt'][$quizName]['details']['pass_mark']) {
+			// Show pass finish message
+			$string = $objTokens->doReplacement($_SESSION['wpsqt'][$quizName]['details']['pass_finish_message']);
+			echo nl2br($string);
+	} else if ( isset($_SESSION['wpsqt'][$quizName]['details']['fail_review']) &&
+		$_SESSION['wpsqt'][$quizName]['details']['fail_review'] == "yes" &&
+		$percentRight < $_SESSION['wpsqt'][$quizName]['details']['pass_mark'] &&
+   		$_SESSION['wpsqt'][$quizName]['details']['finish_display'] != 'Both'){
+			require_once Wpsqt_Core::pageView('site/quiz/review.php');
 	} else if ( isset($_SESSION['wpsqt'][$quizName]['details']['finish_message']) &&
-			  !empty($_SESSION['wpsqt'][$quizName]['details']['finish_message'])) {
+		!empty($_SESSION['wpsqt'][$quizName]['details']['finish_message'])) {
 			// PARSE TOKENS
 			$string = $objTokens->doReplacement($_SESSION['wpsqt'][$quizName]['details']['finish_message']);
 			echo nl2br($string);
-		} else {
-			_e('Thank you for your time.', 'wp-survey-and-quiz-tool');
+	} else {
+		_e('Thank you for your time.', 'wp-survey-and-quiz-tool');
 	} ?>
 		
-<?php } elseif ($_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Quiz Review'){ 
+<?php } if ($_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Quiz Review' || $_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Both'){ 
 	require_once Wpsqt_Core::pageView('site/quiz/review.php');	
 } 
 
