febbfbcbb5580f365ffb5bb9823434f7d67fcc1b
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-12-06 12:52:35 +0000

    Navbar sanity check

--- pages/admin/misc/navbar.php
+++ pages/admin/misc/navbar.php
@@ -30,11 +30,13 @@ if ( isset($_GET['id']) ){
 	<div>
 		<ul class="subsubsub">
 			<li><strong><?php echo $quizName; ?> :</strong></li> 
-			<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=edit&subsection=<?php esc_html_e($_GET["subsection"], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('edit') ) { ?> class="current"<?php }?>>Edit</a> | </li> 
-			<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=sections&subsection=<?php esc_html_e($_GET["subsection"], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('sections') ) { ?> class="current"<?php }?>>Sections</a> | </li> 
-			<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=questions&subsection=<?php esc_html_e($_GET["subsection"], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('questions') ) { ?> class="current"<?php }?>>Questions</a> | </li>  
-			<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=form&subsection=<?php esc_html_e($_GET['subsection'], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('form') ) { ?> class="current"<?php }?>>Form</a> | </li> 
-			<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=results&subsection=<?php esc_html_e($_GET["subsection"], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('results') ) { ?> class="current"<?php }?>>Results</a></li> 
+			<?php if (isset($_GET['subsection'])) { ?>
+				<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=edit&subsection=<?php esc_html_e($_GET["subsection"], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('edit') ) { ?> class="current"<?php }?>>Edit</a> | </li> 
+				<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=sections&subsection=<?php esc_html_e($_GET["subsection"], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('sections') ) { ?> class="current"<?php }?>>Sections</a> | </li> 
+				<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=questions&subsection=<?php esc_html_e($_GET["subsection"], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('questions') ) { ?> class="current"<?php }?>>Questions</a> | </li>  
+				<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=form&subsection=<?php esc_html_e($_GET['subsection'], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('form') ) { ?> class="current"<?php }?>>Form</a> | </li> 
+				<li><a href="<?php echo WPSQT_URL_MAIN; ?>&section=results&subsection=<?php esc_html_e($_GET["subsection"], 'wp-survey-and-quiz-tool'); ?>&id=<?php esc_html_e($_GET["id"], 'wp-survey-and-quiz-tool'); ?>"<?php if ( wpsqt_is_section('results') ) { ?> class="current"<?php }?>>Results</a></li> 
+			<?php } ?>
 			<li style="padding-left: 30px;">Shortcode: <pre style="display: inline;">[wpsqt name="<?php echo $quizName; ?>" type="<?php echo $quizType; ?>"]</pre></li>
 		</ul>
 
