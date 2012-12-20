6d02231f737759b061c5c86c83fc4998445024d8
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-12-20 17:21:50 +0000

    Make time elapsed persistant across sections

--- pages/site/quiz/section.php
+++ pages/site/quiz/section.php
@@ -20,10 +20,17 @@ if (isset($GLOBALS['q_config']) && isset($GLOBALS['q_config']['url_info']['url']
 	</form>
 <?php } ?>
 
+<?php
+if (isset($_POST['wpsqt_time_elapsed'])) {
+	$time_elapsed = $_POST['wpsqt_time_elapsed'];
+} else {
+	$time_elapsed = 0;
+}
+?>
 <form method="post" action="<?php echo esc_url($url); ?>">
 	<input type="hidden" name="wpsqt_nonce" value="<?php echo WPSQT_NONCE_CURRENT; ?>" />
 	<input type="hidden" name="step" value="<?php echo ( $_SESSION['wpsqt']['current_step']+1); ?>" />
-	<input type="hidden" name="wpsqt_time_elapsed" value="0" class="wpsqt_time_elapsed" />
+	<input type="hidden" name="wpsqt_time_elapsed" value="<?php echo $time_elapsed; ?>" class="wpsqt_time_elapsed" />
 <?php
 		$answers = ( isset($_SESSION["wpsqt"][$quizName]["sections"][$sectionKey]["answers"]) ) ? $_SESSION["wpsqt"][$quizName]["sections"][$sectionKey]["answers"] : array();
 foreach ($_SESSION['wpsqt'][$quizName]['sections'][$sectionKey]['questions'] as $questionKey => $question) { ?>
