e832eeab55a3dc0d870edf495864482e31366cca
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-12-20 16:54:24 +0000

    Re-fix contact form fix for p-bar

--- lib/Wpsqt/Shortcode.php
+++ lib/Wpsqt/Shortcode.php
@@ -246,12 +246,8 @@ class Wpsqt_Shortcode {
 			}
 		}
 
-		// Save whether we are using a contact form for the progress bar later
-		$display_contact_form = false;
-
 		// handle contact form and all the stuff that comes with it.
 		if ( isset($_SESSION['wpsqt'][$quizName]['details']['contact']) && $_SESSION['wpsqt'][$quizName]['details']['contact'] == "yes" && $this->_step <= 1 ){
-			$display_contact_form = true;
 			$fields = $wpdb->get_results(
 							$wpdb->prepare("SELECT * FROM `".WPSQT_TABLE_FORMS."` WHERE item_id = %d ORDER BY id ASC",
 							array($_SESSION['wpsqt'][$quizName]['details']['id'])),ARRAY_A
@@ -299,10 +295,6 @@ class Wpsqt_Shortcode {
 		} else {
 			$this->_key = $this->_step;
 		}
-
-
-
-
 
 		// Handles the timer if enabled
 		if ($this->_key == 0) {
@@ -459,7 +451,8 @@ class Wpsqt_Shortcode {
 			if (isset($_SESSION['wpsqt'][$quizName]['details']['show_progress_bar']) && $_SESSION['wpsqt'][$quizName]['details']['show_progress_bar'] == 'yes') {
 				// Progress bar 
 				$current_step = $this->_step + 1;
-				if ($display_contact_form) {
+				// If there is a contact page then loose 1
+				if (isset($_SESSION['wpsqt'][$quizName]['details']['contact']) && $_SESSION['wpsqt'][$quizName]['details']['contact'] == "yes") {
 					$current_step--;
 				}
 				printf(__('Page %d out of %d', 'wp-survey-and-quiz-tool'), $current_step, (sizeof($_SESSION["wpsqt"][$quizName]["sections"])));
