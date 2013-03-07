79d2c3a080d4e0722e2a6b5cdcea0cdbbeef8a8a
Ollie Armstrong <me@olliearmstrong.co.uk>
2013-03-07 14:20:36 +0000

    Class "Exam Finished" text

--- pages/site/quiz/finished.php
+++ pages/site/quiz/finished.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // Set up the token object
 require_once WPSQT_DIR.'/lib/Wpsqt/Tokens.php';
 $objTokens = Wpsqt_Tokens::getTokenObject();
@@ -6,7 +6,7 @@ $objTokens->setDefaultValues();
 
 ?>
 
-<h2><?php _e('Exam Finished', 'wp-survey-and-quiz-tool'); ?></h2>
+<h2 class="wpsqt-exam-finished-title"><?php _e('Exam Finished', 'wp-survey-and-quiz-tool'); ?></h2>
 
 <?php if ($_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Finish message' || $_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Both'  ) { ?>
 	<?php if (isset($_SESSION['wpsqt'][$quizName]['details']['pass_finish']) &&
@@ -28,15 +28,15 @@ $objTokens->setDefaultValues();
 	} else {
 		_e('Thank you for your time.', 'wp-survey-and-quiz-tool');
 	} ?>
-		
-<?php } if ($_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Quiz Review' || $_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Both'){ 
-	require_once Wpsqt_Core::pageView('site/quiz/review.php');	
-} 
+
+<?php } if ($_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Quiz Review' || $_SESSION['wpsqt'][$quizName]['details']['finish_display'] == 'Both'){
+	require_once Wpsqt_Core::pageView('site/quiz/review.php');
+}
 
 	if ( $_SESSION['wpsqt'][$quizName]['details']['use_pdf'] == "yes" ){
 		?>
 		<a href="<?php echo plugins_url('pdf.php?quizid='.$_SESSION['wpsqt'][$quizName]['details']['id'].'&id='.$_SESSION['wpsqt']['result_id'],WPSQT_FILE); ?>"><?php _e('Download certification', 'wp-survey-and-quiz-tool'); ?></a>
-		<?php 
+		<?php
 	}
 ?>
 
