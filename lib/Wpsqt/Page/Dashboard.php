a07404b4a307296beb0e85213f68db3ab0d683bd
pyrosx <pyrosx@gmail.com>
2015-05-11 15:14:40 +1000

    Changed Dashboard to check 5-fails quiz by quiz, instead of all at once, hopefully to fix memory issues

--- lib/Wpsqt/Page/Dashboard.php
+++ lib/Wpsqt/Page/Dashboard.php
@@ -32,57 +32,63 @@ class Wpsqt_Page_Dashboard extends Wpsqt_Page {
 
 		$this->_pageVars['overallCompRate'] = Wpsqt_System::colorCompletionRate( Wpsqt_System::getOverallCompletionRate() );
 
-		$query = $wpdb->get_results(
-		"SELECT u.ID, u.user_email, r.item_id, r.pass, r.status FROM `".WPSQT_TABLE_RESULTS."` r
-		INNER JOIN `".WP_TABLE_USERS."` u ON r.user_id = u.ID", 'ARRAY_A');		
+		$quizzes = $wpdb->get_results(
+		"SELECT id, name FROM `".WPSQT_TABLE_QUIZ_SURVEYS."` WHERE enabled=1",'ARRAY_A');
 		
-		$users = array();
-		$fails = 0;
 		$failDetails = array();
+		$fails = 0;
+
+		foreach($quizzes as $quiz) {
+			
+			$query = $wpdb->get_results(
+			"SELECT u.ID, u.user_email, r.item_id, r.pass, r.status FROM `".WPSQT_TABLE_RESULTS."` r
+			INNER JOIN `".WP_TABLE_USERS."` u ON r.user_id = u.ID
+			WHERE r.item_id = '".$quiz['id']."'", 'ARRAY_A');		
+			
+			unset($users);
+			$users = array();
 		
-		foreach($query as $res) {
-			$failed = false;
+			foreach($query as $res) {
+				$failed = false;
 
-			if(!is_null($res['item_id'])) {
-				$userId = $res['ID'];
-				$quizId = $res['item_id'];
+				if(!is_null($res['item_id'])) {
+					$userId = $res['ID'];
 
-				if (!isset($users[$userId][$quizId]['attempts'])) {
-					$users[$userId][$quizId]['attempts'] = 0;
-					$users[$userId][$quizId]['fails'] = 0;
+					if (!isset($users[$userId]['attempts'])) {
+						$users[$userId]['attempts'] = 0;
+						$users[$userId]['fails'] = 0;
 							
-				}
-				$users[$userId][$quizId]['attempts']++;
-				if ($res['pass'] == 0 && $res['status'] != 'rejected') {
-					$users[$res['ID']][$res['item_id']]['fails']++;
-					if (!$failed && $users[$res['ID']][$res['item_id']]['fails'] >= 5) {
-						$failed = true;
-					}			
-				}
-				
-				if ($failed) {
-					$failDetail['id'] = $res['ID'];
-					$failDetail['email'] = $res['user_email'];
- 					$failDetail['attempts'] = $users[$res['ID']][$res['item_id']]['attempts'];
-
-					$res2 = $wpdb->get_results("SELECT * FROM `".WPSQT_TABLE_EMPLOYEES."` e INNER JOIN `".WPSQT_TABLE_STORES."` s ON s.id = e.id_store WHERE e.id_user = ".$res['ID'],'ARRAY_A');
-					$failDetail['store'] = "";
-					foreach($res2 as $store) {
-					$failDetail['store'] .= $store['location'].", ";
 					}
-					$failDetail['store'] = substr($failDetail['store'], 0, -2); // chop off last ", "
+					
+					$users[$userId]['attempts']++;
+					if ($res['pass'] == 0 && $res['status'] != 'rejected') {
+						$users[$userId]['fails']++;
+						if (!$failed && $users[$userId]['fails'] >= 5) {
+							$failed = true;
+						}			
+					}
+				
+					if ($failed) {
+						$failDetail['id'] = $userId;
+						$failDetail['email'] = $res['user_email'];
+						$failDetail['attempts'] = $users[$userId]['attempts'];
 
-					$failDetail['id_quiz'] = $res['item_id'];
-					$failDetail['quiz'] = $wpdb->get_var("SELECT name FROM `".WPSQT_TABLE_QUIZ_SURVEYS."` WHERE id = ".$res['item_id']);
+						$res2 = $wpdb->get_results("SELECT * FROM `".WPSQT_TABLE_EMPLOYEES."` e INNER JOIN `".WPSQT_TABLE_STORES."` s ON s.id = e.id_store WHERE e.id_user = ".$userId,'ARRAY_A');
+						$failDetail['store'] = "";
+						foreach($res2 as $store) {
+							$failDetail['store'] .= $store['location'].", ";
+						}
+						$failDetail['store'] = substr($failDetail['store'], 0, -2); // chop off last ", "
 
-					$fails++;
-					$failDetails[] = $failDetail;
+						$failDetail['id_quiz'] = $quiz['id'];
+						$failDetail['quiz'] = $quiz['name'];
 
+						$fails++;
+						$failDetails[] = $failDetail;
+					}
 				}
-
 			}
 		}
-
 
 //		print("<pre>");
 //		var_dump($query);
